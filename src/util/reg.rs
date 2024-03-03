efaf71ec04af0f6211db26e5b6f0feef3c66a299
Ethan Green <ethangreen.dev@gmail.com>
2024-03-03 15:30:27 -0500

    shim util::reg pending legit wine impl

--- src/util/reg.rs
+++ src/util/reg.rs
@@ -1,4 +1,4 @@
-use winreg::RegKey;
+pub use inner::*;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -19,36 +19,64 @@ pub struct RegKeyVal {
     pub val: String,
 }
 
-pub fn get_value_at(hkey: HKey, subkey: &str, name: &str) -> Result<String, Error> {
-    open_subkey(hkey, subkey)?
-        .get_value(name)
-        .map_err(|_| Error::RegistryValueRead(subkey.to_string(), name.to_string()))
+#[cfg(target_os = "windows")]
+mod inner {
+    use winreg::RegKey;
+
+    use super::{Error, HKey, RegKeyVal};
+
+    pub fn get_value_at(hkey: HKey, subkey: &str, name: &str) -> Result<String, Error> {
+        open_subkey(hkey, subkey)?
+            .get_value(name)
+            .map_err(|_| Error::RegistryValueRead(subkey.to_string(), name.to_string()))
+    }
+
+    pub fn get_keys_at(hkey: HKey, subkey: &str) -> Result<Vec<String>, Error> {
+        open_subkey(hkey, subkey)?
+            .enum_keys()
+            .collect::<Result<Vec<String>, _>>()
+            .map_err(|_| Error::RegistrySubkeyRead(subkey.to_string()))
+    }
+
+    pub fn get_values_at(hkey: HKey, subkey: &str) -> Result<Vec<RegKeyVal>, Error> {
+        open_subkey(hkey, subkey)?
+            .enum_values()
+            .map(|x| match x {
+                Ok((key, val)) => Ok(RegKeyVal {
+                    key,
+                    val: val.to_string(),
+                }),
+                Err(e) => Err(e),
+            })
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|_| Error::RegistrySubkeyRead(subkey.to_string()))
+    }
+
+    fn open_subkey(hkey: HKey, subkey: &str) -> Result<RegKey, Error> {
+        let local = RegKey::predef(hkey as _);
+        local
+            .open_subkey(subkey)
+            .map_err(|_| Error::RegistrySubkeyRead(subkey.to_string()))
+    }
 }
 
-pub fn get_keys_at(hkey: HKey, subkey: &str) -> Result<Vec<String>, Error> {
-    open_subkey(hkey, subkey)?
-        .enum_keys()
-        .collect::<Result<Vec<String>, _>>()
-        .map_err(|_| Error::RegistrySubkeyRead(subkey.to_string()))
-}
+#[cfg(target_os = "linux")]
+mod inner {
+    use super::{Error, HKey, RegKeyVal};
 
-pub fn get_values_at(hkey: HKey, subkey: &str) -> Result<Vec<RegKeyVal>, Error> {
-    open_subkey(hkey, subkey)?
-        .enum_values()
-        .map(|x| match x {
-            Ok((key, val)) => Ok(RegKeyVal {
-                key,
-                val: val.to_string(),
-            }),
-            Err(e) => Err(e),
-        })
-        .collect::<Result<Vec<_>, _>>()
-        .map_err(|_| Error::RegistrySubkeyRead(subkey.to_string()))
-}
+    fn get_prefix() -> Option<String> {
+        todo!()
+    }
 
-fn open_subkey(hkey: HKey, subkey: &str) -> Result<RegKey, Error> {
-    let local = RegKey::predef(hkey as _);
-    local
-        .open_subkey(subkey)
-        .map_err(|_| Error::RegistrySubkeyRead(subkey.to_string()))
+    pub fn get_value_at(hkey: HKey, subkey: &str, name: &str) -> Result<String, Error> {
+        todo!()
+    }
+
+    pub fn get_keys_at(hkey: HKey, subkey: &str) -> Result<Vec<String>, Error> {
+        todo!()
+    }
+
+    pub fn get_values_at(hkey: HKey, subkey: &str) -> Result<Vec<RegKeyVal>, Error> {
+        todo!()
+    }
 }
