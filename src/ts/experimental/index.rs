a4b86c0954f64b0bbb980df5bebe181640a35bc6
Ethan Green <ethangreen.dev@gmail.com>
2024-03-20 11:04:49 -0400

    implement additional index query methods

--- src/ts/experimental/index.rs
+++ src/ts/experimental/index.rs
@@ -1,11 +1,11 @@
-use std::path::Path;
+use std::time::SystemTime;
 
 use async_compression::futures::bufread::GzipDecoder;
+use chrono::{DateTime, FixedOffset, NaiveDateTime, TimeZone};
 use futures::{TryStreamExt, AsyncBufReadExt, StreamExt};
 use futures::io::{self, BufReader, ErrorKind};
 use futures_util::Stream;
 use serde::{Serialize, Deserialize};
-use tokio::fs::OpenOptions;
 
 use crate::Error;
 use crate::ts::package_reference::PackageReference;
@@ -69,4 +69,46 @@ pub async fn get_index_streamed() -> Result<impl Stream<Item = Result<PackageInd
 		});
 
 	Ok(lines)
+}
+
+pub async fn get_index_streamed_raw() -> Result<impl Stream<Item = Result<String, Error>>, Error> {
+	let response = CLIENT
+		.get(format!("{EX}/package-index"))
+		.send().await?
+		.error_for_status()?;
+
+	let reader = response
+		.bytes_stream()
+		.map_err(|e| io::Error::new(ErrorKind::Other, e))
+		.into_async_read();
+
+	let lines = BufReader::new(GzipDecoder::new(reader))
+		.lines()
+		.map(|x| match x {
+			Ok(x) => Ok(x),
+			Err(e) => Err(Error::GenericIoError(e))
+		});
+
+	Ok(lines)
+}
+
+pub async fn get_index_update_time() -> Result<NaiveDateTime, Error> {
+	let response = CLIENT
+		.head(format!("{EX}/package-index"))
+		.send().await?
+		.error_for_status()?;
+
+	let headers = response.headers();
+	let date = headers
+		.get("date")
+		.expect("Failed to parse date from package-index API header.")
+		.to_str()
+		.unwrap();
+
+	//  Fri, 15 Mar 2024 18:49:19 GMT
+	let date_format = "%a, %d %b %Y %H:%M:%S %Z";
+	let date = NaiveDateTime::parse_from_str(date, date_format)
+		.unwrap_or_else(|e| panic!("Failed to parse datetime '{date}': {e:?}"));
+
+	Ok(date)
 }
